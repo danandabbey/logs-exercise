b7fb1bf75b5b6060c800801c3fd9a44ff329c429
Daniel Frederick <danandabbey502@gmail.com>
2024-12-12 23:19:40 -0500

    Refactor error extraction and implement file writing for error logs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
 use std::fs;
+use std::io::Error;
 
-fn extract_errors(text: &str) -> Vec<&str> {
-    let new_text = text.split("/n");
+fn extract_errors(text: &str) -> Vec<String> {
     let mut results = vec![];
 
-    for lines in new_text {}
-}
-
-fn main() {
-    match fs::read_to_string("logs.txt") {
-        Ok(value) => {
-            println!("{:#?}", value.len())
-        }
-        Err(value) => {
-            println!("{:#?}", value)
+    for line in text.split("\n") {
+        if line.starts_with("ERROR") {
+            results.push(String::from(line));
         }
     }
+    results
+}
+
+fn main() -> Result<(), Error> {
+    let text = fs::read_to_string("logs.txt")?;
+    let results = extract_errors(text.as_str());
+    fs::write("errors.txt", results.join("\n"))?;
+
+    Ok(())
+
+    // match fs::read_to_string("logs.txt") {
+    //     Ok(value) => {
+    //         let results = extract_errors(value.as_str());
+
+    //         match fs::write("errors.txt", results.join("\n")) {
+    //             Ok(..) => println!("success"),
+    //             Err(value) => print!("{:#?}", value),
+    //         }
+    //     }
+    //     Err(value) => {
+    //         println!("{:#?}", value)
+    //     }
+    // }
 }
